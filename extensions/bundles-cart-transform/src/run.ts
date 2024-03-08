95e0c2c91631cd3feeb880e45ed968f6d3ed2596
Hannah Robinson <hannah.robinson.git@gmail.com>
2024-03-09 12:14:30 +1300

    Add bundle logic to Function

--- extensions/bundles-cart-transform/src/run.ts
+++ extensions/bundles-cart-transform/src/run.ts
@@ -1,12 +1,37 @@
 import type {
   RunInput,
   FunctionRunResult,
+  CartLine,
+  CartOperation
 } from "../generated/api";
 
-const NO_CHANGES: FunctionRunResult = {
-  operations: [],
-};
-
 export function run(input: RunInput): FunctionRunResult {
-  return NO_CHANGES;
+  const groupedItems: Record<string, Pick<CartLine, "id" | "quantity">[] > = {};
+  
+  input.cart.lines.forEach(line => {
+    const bundleId = line.bundleId;
+    if(bundleId && bundleId.value){
+      if(!groupedItems[bundleId.value]){
+        groupedItems[bundleId.value] = []
+      }
+      groupedItems[bundleId.value].push(line);
+    }
+  })
+  return {
+    operations: [
+      ...Object.values(groupedItems).map(group => {
+        const mergeOperation: CartOperation = {
+          merge: {
+            cartLines: group.map(line => {
+              return { cartLineId: line.id,
+                quantity: line.quantity
+              }
+            }),
+            parentVariantId: "gid://shopify/ProductVariant/48071724564782"  
+          }
+        }
+        return mergeOperation
+      })
+    ]
+  };
 };
